2ecc578c4e4aaf342772234881bccdb473ac34fb
Thomas Sigler <ts309435@gmail.com>
2024-04-15 22:06:08 -0400

    Set up hyperparameter tuning

--- test_proj.py
+++ test_proj.py
@@ -12,6 +12,13 @@ import math as m
 from PIL import Image as I
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix as cm
+
+from ray import train, tune
+from ray.tune.schedulers import ASHAScheduler
+from ray.train import Checkpoint
+from ray.tune.search.optuna import OptunaSearch
 
 #Model Initialization, Layer Replacement, and Gradient Changes
 x = resnet50(weights=ResNet50_Weights.DEFAULT)
@@ -42,15 +49,16 @@ def load_images():
     test_labels = []
     val_imgs = []
     val_labels = []
+    idx = 0
 
     crop = T.CenterCrop((128, 128))
 
     #Cycle through various folders for images
-    for folder in range(len(sorted(os.listdir('./Data')))):
-        if folder == 0 or os.listdir('./Data')[folder] == '.DS_Store':
+    for folder in range(len(sorted(os.listdir('./DeepLearningProject/Data')))):
+        if os.listdir('./DeepLearningProject/Data')[folder] == '.DS_Store':
             continue
         else:
-            aux_img = os.listdir('./Data/'+os.listdir('./Data')[folder])
+            aux_img = os.listdir('./DeepLearningProject/Data/'+os.listdir('./DeepLearningProject/Data')[folder])
 
             for i in range(len(aux_img)):
                 #Remove .DS_Store file from data
@@ -61,20 +69,22 @@ def load_images():
             #Resize Image and Convert Channels
             for i in range(len(aux_img)):
                 try:
-                    with I.open('./Data/'+os.listdir('./Data')[folder]+'/'+aux_img[i]) as img:
+                    with I.open('./DeepLearningProject/Data/'+os.listdir('./DeepLearningProject/Data')[folder]+'/'+aux_img[i]) as img:
                         img = crop(img)
 
                         if img.mode != 'RGB':
                             img = img.convert('RGB')
 
                         img = np.array(img)
-                        #img = np.stack([img]*3, axis=-1)
                         aux_img[i] = img
                 except Exception as e:
                     print(f"{i}")
+            aux_labels = []
 
             #Generate Labels
-            aux_labels = [(folder-1) for i in range(len(aux_img))]
+            for i in range(len(aux_img)):
+                aux_labels.append(idx)
+            idx += 1
 
             #Divide Data and Labels into Training, Testing, and Validation Sets
             train_imgs += aux_img[:int(m.ceil(len(aux_img)*0.6))]
@@ -108,8 +118,6 @@ def load_images():
     test_imgs, test_labels = shuffle_imgs(test_imgs, test_labels)
     val_imgs, val_labels = shuffle_imgs(val_imgs, val_labels)
 
-    print(val_labels.shape)
-
     #Normalize and Reorder Data for Processing
     train_imgs = normalize(train_imgs.permute(0, 3, 1, 2))
     test_imgs = normalize(test_imgs.permute(0, 3, 1, 2))
@@ -117,7 +125,7 @@ def load_images():
 
     train_set, test_set, val_set = TensorDataset(train_imgs, train_labels), TensorDataset(test_imgs, test_labels), TensorDataset(val_imgs, val_labels)
 
-    trainloader, testloader, valloader = DataLoader(dataset=train_set, shuffle=True, batch_size=128), DataLoader(dataset=test_set, shuffle=True, batch_size=128), DataLoader(dataset=val_set, shuffle=True, batch_size=128)
+    trainloader, testloader, valloader = DataLoader(dataset=train_set, shuffle=True, batch_size=128), DataLoader(dataset=test_set, shuffle=True, batch_size=64), DataLoader(dataset=val_set, shuffle=True, batch_size=64)
 
     return trainloader, testloader, valloader
 
@@ -128,8 +136,8 @@ def shuffle_imgs(imgs, labels):
    return imgs[shuffle], labels[shuffle]
 
 #Training Method with Adam Optimizer
-def train(model, traindata, valdata, epochs):
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+def trainfunc(model, traindata, valdata, epochs):
+    optimizer = optim.Adam(model.parameters(), lr=0.006349288161821683, eps=6.5131448417080975e-09)
     lr_optim = optim.lr_scheduler.StepLR(optimizer, step_size=2)
     loss_list = []
     loss_list_aux = []
@@ -216,14 +224,107 @@ def test(model, data):
         total += (pred == labels).float().sum().item()
     print("Accuracy: ", total/div*100, "%")
 
+def confusion_matrix(model, data):
+    total_pred = []
+    total_labels = []
+
+    for imgs, labels in data:
+        imgs = imgs.to(device)
+        labels = labels.to(device)
+
+        logits = model(imgs)
+        logits = nn.Softmax()(logits)
+        pred = torch.argmax(logits, axis=1)
+
+        total_pred += list(pred.to('cpu'))
+        total_labels += list(labels.to('cpu'))
+
+    con_mat = cm(total_labels, total_pred)
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(con_mat, annot=True, cmap="inferno")
+    plt.title(f'ResNet50 Accuracy')
+    plt.ylabel('True Labels')
+    plt.xlabel('Predicted Labels')
+    plt.show()
+    plt.clf()
+
+def hfinetunetrain(model, data, optimizer):
+    for imgs, labels in data:
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        pred = model(imgs)
+        optimizer.zero_grad()
+        loss = F.cross_entropy(pred, labels)
+        loss.backward()
+        optimizer.step()
+    torch.mps.empty_cache()
+
+def hfinetunetest(model, data):
+    model.eval()
+    correct = 0
+    total = 0
+
+    for imgs, labels in data:
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        outputs = model(imgs)
+        
+        pred = torch.argmax(outputs, axis=1)
+
+        total += imgs.shape[0]
+        correct += (pred == labels).float().sum().item()
+    torch.mps.empty_cache()
+    return correct/total
+
+def objective(config):
+    train_loader, test_loader, val_loader = load_images()
+
+    model = resnet50(weights=ResNet50_Weights.DEFAULT).to(device)
+    for param in x.parameters():
+        param.requires_grad = False
+
+    model.fc = nn.Sequential(nn.Linear(2048, config["l1"]),
+                                       nn.ReLU(),
+                                       nn.Linear(config["l1"], config["l2"]),
+                                       nn.ReLU(),
+                                       nn.Linear(config["l2"], 12)
+                                       ).to(device)
+    nn.init.kaiming_uniform_(x.fc.weight, nonlinearity='relu')
+    x.fc.requires_grad = True
+
+    optimizer = optim.Adam(model.parameters(), lr=config["lr"], eps=config["eps"])
+
+
+    for epoch in range(10):
+        hfinetunetrain(model, train_loader, optimizer)
+        acc = hfinetunetest(model, val_loader)
+        
+        checkpoint = None
+        if (epoch+1)%5 == 0:
+            torch.save(model.state_dict(),
+                       "./DeepLearningProject/TuningCheckpoints/model.pth")
+            checkpoint = Checkpoint.from_directory("./DeepLearningProject/TuningCheckpoints")
+        train.report({"mean_accuracy": acc}, checkpoint=checkpoint)
+        torch.mps.empty_cache()
+
+search_space = {"lr": tune.loguniform(1e-4, 1e-1), "eps": tune.loguniform(1e-10, 1e-6), "l1": tune.randint(50, 2048), "l2": tune.randint(12, 100)}
+tuner = tune.Tuner(objective, tune_config=tune.TuneConfig(metric="mean_accuracy", mode="max", search_alg=OptunaSearch(), num_samples=2), param_space=search_space)
+
+results = tuner.fit()
+print("Best Config is: ", results.get_best_result().config)
+
 trainloader, testloader, valloader = load_images()
 test(x, testloader)
 
-x, loss_list = train(x, trainloader, valloader, 200)
+confusion_matrix(x, testloader)
+
+x, loss_list = trainfunc(x, trainloader, valloader, 50)
 test(x, testloader)
 
+confusion_matrix(x, testloader)
+
 #Finetuning and testing Finetuning
-x, loss_list_val = finetune(x, trainloader, valloader, 200)
+x, loss_list_val = finetune(x, trainloader, valloader, 50)
 test(x, testloader)
 
 #Training Loss vs. Epochs Plot
